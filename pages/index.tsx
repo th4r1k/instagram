3d948883549e613eae57ebeb490420cd1c95ea67
unknown <th4r1k@gmail.com>
2022-05-27 07:34:35 -0300

    intagram lp

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,101 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
 
 const Home: NextPage = () => {
+  const[mounted, setMounted] = useState(false)
+  const {theme, setTheme} = useTheme()
+
+  useEffect(()=> setMounted(true),[])
+  if(!mounted) return null
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <>
+    
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Instagram</title>
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      
+      <div className="flex h-screen flex-col items-center bg-slate-100 dark:bg-slate-800 dark:text-white">
+      <button className="mt-4 rounded-lg px-4 py-2 bg-slate-800 dark:bg-white text-white dark:text-black"
+      onClick={() => setTheme(theme === 'light'? 'dark' :'light')}
+      >
+      {theme === 'light' ? 'Dark' : 'Light'}
+      </button>
+      <main className="flex h-4/5 items-center">
+        
+        <div className="hidden shirink-0 sm:flex items-center ">
+          <Image src="/border.png" width={420} height={580} />
+          
         </div>
-      </main>
+        
+        <div className='columns-1 content-center '> 
+          <div className='flex flex-col border-2 align-middle px-10 pt-10 bg-white flex-shrink-0 dark:bg-slate-800 dark:text-white' >
+            <img className='px-10 py-5 hidden dark:flex' src='/logow.png' /> 
+            <img className='px-10 py-5 dark:hidden' src='/logo.png' /> 
+            <input className='rounded-sm border-r-2 border-2 mt-5 p-2 mb-1 text-xs w-full bg-slate-100 dark:bg-slate-800 dark:text-white' type="text" placeholder="Telefone, usuário ou email" />
+            <input className='rounded-sm border-2 text-xs p-2 mb-2 w-auto bg-slate-100 dark:bg-slate-800 dark:text-white' type="password" placeholder="Senha" />
+            <button className='bg-blue-300 rounded-xs mb-3 text-white text-sm p-1' > Entrar </button>
+         
+          <div className='flex justify-between '>
+            <span className='w-full border-t-2 self-center'></span>
+            <span className='text-xs text-opacity-75 text-slate-500 bold align-top'>OU</span>
+            <span className='w-full border-t-2 self-center'></span>
+          </div>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+          <div className='flex-col'>
+            <button className='w-full mt-4 text-xs justify-center align-middle content-center self-center text-blue-800 font-bold'>Entrar com o Facebook </button>
+            <button className='w-full mt-4 text-xs self-center mb-5'>Esqueceu a senha? </button>
+          </div>
+        </div>
+                  
+        <div className='text-xs mt-2 border-2 flex-col bg-white rounded-sm dark:bg-slate-800 dark:text-white'>
+          <p className='flex align-middle justify-center p-5'> Não tem uma conta? <a href='#' className='text-blue-600 ml-2'>Cadastre-se</a></p>
+        </div>
+   
+          <span className='text-xs flex justify-center py-2'> Obtenha o Aplicativo</span>
+        
+        <div className='flex justify-center align-middle '>
+        <Image src="/appstore.png" width={136} height={40} />
+        <Image src="/playstore.png" width={136} height={40} />
+        </div>
+      </div>
+      </main>
+               
+      <div className="flex w-fit justify-center  h-fit text-xs text-gray-500 flex-wrap  ">
+       <a className='mx-2' href=''>Meta </a>
+       <a className='mx-2' href=''>Sobre </a>
+       <a className='mx-2' href=''>Blog </a>
+       <a className='mx-2' href=''>Carreiras </a>
+       <a className='mx-2' href=''>Ajuda</a>
+       <a className='mx-2' href=''>API </a>
+       <a className='mx-2' href=''>Privacidade </a>
+       <a className='mx-2' href=''>Termos </a>
+       <a className='mx-2' href=''>Principais contas </a>
+       <a className='mx-2' href=''>Hastags</a>
+       <a className='mx-2' href=''>Localizações</a>
+       <a className='mx-2' href=''>Instagram Lite </a>
+      </div>
+   
+      <div  className="flex w-fit justify-center my-2  h-fit text-xs text-gray-500 flex-wrap">
+       <a className='mx-2' href=''>Dança </a>
+       <a className='mx-2' href=''>Comida e bebida</a>
+       <a className='mx-2' href=''>Casa e jardim</a>
+       <a className='mx-2' href=''>Música </a>
+       <a className='mx-2' href=''>Artes visuais </a>
+       </div>
+
+       <div  className="flex w-fit justify-center my-3  h-fit text-xs text-gray-500 flex-wrap">
+         <select className='bg-transparent' name="Português (Brasil)">
+          <option> Português (Brasil)</option>
+         </select>
+         <p>© 2022 Instagram from Meta</p>
+        </div>
+        
     </div>
+    </>
   )
 }
 
